66aeb9b672de38686858686ab7b1da8864f95850
vegito165 <nguyentanhau1651997@gmail.com>
2017-11-18 05:15:00 +0700

    v

---
+++ main.js
@@ -0,0 +1,32 @@
+function openStream(){
+  const config = {audio : false, video: true};
+  return navigator.mediaDevices.getUserMedia(config);
+}
+
+function playStream(idVideoTag,stram){
+  const video = document.getElementById(idVideoTag);
+  video.srcObject = stram;
+  video.play();
+}
+
+// openStream().then(stream=>playStream('localStream',stream));
+const peer = new Peer({key: 'lwjd5qra8257b9'});
+peer.on('open',id=>$("#yourid").append(id));
+
+$("#btnCall").on('click',function(){
+  const id = $("#remoteID").val();
+  openStream().then(stream=>{
+    playStream('localStream',stream);
+    const call = peer.call(id,stream);
+    call.on('stream',remoteStream=>playStream('remoteStream',remoteStream));
+  });
+});
+
+peer.on('call', call => {
+  openStream()
+  .then(stream=>{
+    call.answer(stream);
+    playStream('localStream',stream);
+    call.on('stream',remoteStream=>playStream('remoteStream',remoteStream));
+  });
+});
